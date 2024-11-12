aaccacd8a21b3d430c8c836865257450803c8870
IshaanUCC <ishaan.arakkal@gmail.com>
2024-11-12 12:13:23 +0000

    Initial Dev Cycle

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class ColourTable {
+
+    private final int capacity;
+    private final ArrayList<ArrayList<Integer>> colours;
+
+
+    public ColourTable(int capacity){
+        if ( capacity <= 1 || (capacity & (capacity -1 )) != 0) {
+            throw new IllegalArgumentException("Capacity has to be a Power of 2");
+        }
+
+        this.capacity = capacity;
+        this.colours = new ArrayList<ArrayList<Integer>>(capacity);
+    }
+
+    public void add(ArrayList<Integer> colour) {
+        for (Integer bit : colour) {
+            if (bit > 255 || bit < 0) {
+                throw new IllegalStateException("Colour must be a value between 0 and 255");
+            }
+        }
+
+        if (colours.size() >= capacity) {
+            throw new IllegalStateException("Cannot add more colours. Capacity Reached");
+        }
+
+        colours.add(colour);
+        System.out.println("ColourTable: " + colours.toString());
+    }
+
+    public int size(){
+        return colours.size();
+    }
 }
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ColourTableTest {
+import java.util.ArrayList;
+
+class ColourTableTest {
+    @Test
+    void shouldInitializePaletteWithValidSize() {
+        ColourTable table = new ColourTable(4);
+    }
+
+    @Test
+    void shouldThrowExceptionForNowPowerOfTwoSizes() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(3));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(5));
+    }
+
+    @Test
+    void shouldAddColourToPalette() {
+        ColourTable table = new ColourTable(2);
+        ArrayList<Integer> testList = new ArrayList<Integer>();
+        testList.add(255);
+        testList.add(255);
+        testList.add(255);
+        table.add(testList);
+        assertEquals(1, table.size());
+    }
+
+    @Test
+    void shouldThrowExceptionForExceedingBitSize() {
+        ColourTable table = new ColourTable(2);
+        ArrayList<Integer> testList = new ArrayList<Integer>();
+        testList.add(255);
+        testList.add(255);
+        testList.add(256);
+        assertThrows(IllegalStateException.class, () -> table.add(testList));
+    }
+
+    @Test
+    void shouldThrowExceptionForExceedingCapacity() {
+        ColourTable table = new ColourTable(2);
+
+        ArrayList<Integer> testList1 = new ArrayList<Integer>();
+        testList1.add(255);
+        testList1.add(5);
+        testList1.add(25);
+        table.add(testList1);
+
+        ArrayList<Integer> testList2 = new ArrayList<Integer>();
+        testList2.add(67);
+        testList2.add(2);
+        testList2.add(9);
+        table.add(testList2);
+
+        ArrayList<Integer> testList3 = new ArrayList<Integer>();
+        testList3.add(67);
+        testList3.add(2);
+        testList3.add(9);
+        assertThrows(IllegalStateException.class,() -> table.add(testList3));
+    }
 }
